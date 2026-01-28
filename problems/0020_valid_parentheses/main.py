24d6f7b5499afa2bfa62c352d3d1c54bfe9fca95
Gab <53164692+gab-lee@users.noreply.github.com>
2026-01-28 11:35:38 +0800

    completed 0020

---
+++ problems/0020_valid_parentheses/main.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        parentheses = {')': '(', ']': '[', '}': '{'}
+
+        for ch in s:
+            if ch in parentheses.values():
+                stack.append(ch)
+                print(stack)
+            else: #closing 
+                if not stack: #no opening to pair with
+                    return False
+                if stack.pop() != parentheses[ch]:
+                    return False
+        
+        return not stack 
+
+       
+if __name__ == "__main__":
+    s = Solution()
+
+    print(s.isValid("()"))#True
+    print(s.isValid("()[]{}"))#True
+    print(s.isValid("(]"))#False
+    print(s.isValid("([])"))#True
+    print(s.isValid("([)]"))#False
+    print(s.isValid("("))#False
+    print(s.isValid("]["))#False
+    print(s.isValid("(([]){})"))#True
