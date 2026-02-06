021c70feed31619202be76cd45770f42f91827e8
Gab <53164692+gab-lee@users.noreply.github.com>
2026-02-06 15:52:47 +0800

    updated 0217

--- problems/0217_contains_duplciate/main.py
+++ problems/0217_contains_duplciate/main.py
@@ -1,10 +1,7 @@
 from typing import List
 class Solution: 
     def containsDuplicate(self, nums: List[int]) -> bool:
-        """
-        Using a set iterate through nums to keep a list of seen elements
-        return true if current element is in seen
-        """
+        
         seen = set()
         for n in nums:
             if n not in seen:
