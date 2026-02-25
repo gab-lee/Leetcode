a7bf4dcc29230fe4d16976bbab7a3410cbae632d
Gab <53164692+gab-lee@users.noreply.github.com>
2026-02-25 21:25:40 +0800

    unsolved

--- problems/0006_zigzag_conversion/main.py
+++ problems/0006_zigzag_conversion/main.py
@@ -1,9 +1,45 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        pattern = 2*numRows-2
+        """
+        row 1 and row n will have the same number of characters 
+        A Zig (down and diagonal up) is made up of n + n-2 characters 
+        n sequence will look like
+        ```
+        0      6
+        1    5 7 
+        2  4   8 
+        3      9
+        
+        next number is a triangle away 
+
+        numsRow - row 
+        ```
+        """
+        def nextCharacter(self,character_index,numRows):
+            sequence = 2*numRows -2 
+            pos = character_index % sequence
+            if pos <= numRows -1:
+                next_pos = character_index + 2*(numRows-1-pos)
+            else:
+                next_pos = character_index + 2*(sequence+1-pos)
+            print(next_pos)
+            return next_pos
+
+
         if len(s) <= 2: 
             return s
+
+        conversion = ""
         
+        row = 0
+        for i in range (len(s)):
+            if nextCharacter(self,i,numRows) < len(s):
+                conversion += s[nextCharacter(self,i,numRows)]
+            else:
+                row +=1
+                conversion += s[row]
+
+        return conversion
 
 if __name__ == "__main__":
     s = Solution()
